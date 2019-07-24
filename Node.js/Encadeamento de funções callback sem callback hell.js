b5eae353a6165ab13cdcff84835fac915367b0a0
Rafael Teixeira <fael.teixeira@gmail.com>
2019-07-24 04:39:22 -0300

    Adição de comentários no tutorial de como evitar callback hell

--- Node.js/Encadeamento de funções callback sem callback hell.js
+++ Node.js/Encadeamento de funções callback sem callback hell.js
@@ -1,12 +1,14 @@
 var fs=require('fs');
 var arq='test.txt';
 
+// Caso seja um arquivo, e exista, lerá-o e mostrará no console.
 function LerArquivo(erro,dados){
 	if(erro)
 		throw erro;
 	console.log(dados);
 }
 
+// Função callback que testa se é uma arquivo mesmo.
 function Status(erro,tipo){
 	if(erro)
 		throw erro;
@@ -14,9 +16,14 @@ function Status(erro,tipo){
 		fs.readFile(arq,'utf8',LerArquivo);
 }
 
+// Função callback que testa se existe o arquivo.
 function ArquivoExiste(existe){
 	if(existe)
 		fs.stat(arq,Status);
 }
 
 fs.exists(arq,ArquivoExiste);
+
+/* Separando as funções callback, e programando-as
+	uma de cada vez, sem haver encadeamento, o código
+	fica mais fácil de ser entendido.*/
