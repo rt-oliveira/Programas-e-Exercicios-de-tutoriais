1e189f08dd3cbdb1d4c993d4bfdf3390d1e15955
Rafael Teixeira <fael.teixeira@gmail.com>
2019-07-26 04:55:28 -0300

    Add comentários em Sequência de escape (C#)

--- C#/Sequência de escape.cs
+++ C#/Sequência de escape.cs
@@ -3,6 +3,8 @@ using System;
 namespace CaractereEscape{
 	class Program{
 		static void Main(string[] args){
+			/* Nessa impressão são vistos 2 sequências de escape, \n e \t, 
+				que respectivamente indicam nova linha (quebra de linha) e um TAB.*/
 			Console.WriteLine("Hello\tWorld\n\n");
 			Console.ReadLine();
 		}
