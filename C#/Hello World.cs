7c5f1d16dee95ebbb846a0860a85c6d6efca3855
Rafael Teixeira <fael.teixeira@gmail.com>
2019-07-26 02:41:06 -0300

    Exemplo do Hello world, em C#

---
+++ C#/Hello World.cs
@@ -0,0 +1,12 @@
+using System;
+
+// Exemplo da aplicação mais simples que há.
+
+namespace AplicacaoHelloWorld{
+	class HelloWorld{
+		static void Main(string[] args){
+			Console.WriteLine("Hello World");
+			Console.ReadKey();
+		}
+	}
+}
