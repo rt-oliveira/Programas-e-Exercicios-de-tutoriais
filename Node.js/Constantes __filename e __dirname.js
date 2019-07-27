104a22a57755c977a6ac5363e647857b625daf16
Rafael Teixeira <fael.teixeira@gmail.com>
2019-07-27 04:28:23 -0300

    Add exemplo das constantes __filename e __dirname, no node

---
+++ Node.js/Constantes __filename e __dirname.js
@@ -0,0 +1,6 @@
+/*Existem duas constantes, chamadas __filename e __dirname,
+	que significam, respectivamente, o caminho completo do arquivo atual,
+  e o caminho do arquivo, a sua localização.
+*/
+console.log("Nome do arquivo: "+__filename);
+console.log("Localização: "+__dirname);
