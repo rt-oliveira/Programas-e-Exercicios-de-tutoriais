c64af5a49fd038a79086abc9a75171f0eeb1d1d9
Rafael Teixeira <fael.teixeira@gmail.com>
2019-09-06 05:01:29 -0300

    Add projeto Switch Case (Fundação Bradesco)

---
+++ Projetos/Switch_case/Form2.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Switch_case
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblRes = new System.Windows.Forms.Label();
+            this.cboOpcao = new System.Windows.Forms.ComboBox();
+            this.btnConsultar = new System.Windows.Forms.Button();
+            this.btnVoltar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(141, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(197, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Rodízio de Veículos";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(83, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(313, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Selecione o último número da placa";
+            // 
+            // lblRes
+            // 
+            this.lblRes.AutoSize = true;
+            this.lblRes.Location = new System.Drawing.Point(102, 206);
+            this.lblRes.Name = "lblRes";
+            this.lblRes.Size = new System.Drawing.Size(0, 13);
+            this.lblRes.TabIndex = 2;
+            // 
+            // cboOpcao
+            // 
+            this.cboOpcao.FormattingEnabled = true;
+            this.cboOpcao.Items.AddRange(new object[] {
+            "1 ou 2",
+            "3 ou 4",
+            "5 ou 6",
+            "7 ou 8",
+            "9 ou 0"});
+            this.cboOpcao.Location = new System.Drawing.Point(179, 156);
+            this.cboOpcao.Name = "cboOpcao";
+            this.cboOpcao.Size = new System.Drawing.Size(121, 21);
+            this.cboOpcao.TabIndex = 3;
+            this.cboOpcao.SelectedIndexChanged += new System.EventHandler(this.CboOpcao_SelectedIndexChanged);
+            // 
+            // btnConsultar
+            // 
+            this.btnConsultar.Location = new System.Drawing.Point(129, 280);
+            this.btnConsultar.Name = "btnConsultar";
+            this.btnConsultar.Size = new System.Drawing.Size(75, 37);
+            this.btnConsultar.TabIndex = 4;
+            this.btnConsultar.Text = "Consultar";
+            this.btnConsultar.UseVisualStyleBackColor = true;
+            this.btnConsultar.Click += new System.EventHandler(this.BtnConsultar_Click);
+            // 
+            // btnVoltar
+            // 
+            this.btnVoltar.Location = new System.Drawing.Point(275, 280);
+            this.btnVoltar.Name = "btnVoltar";
+            this.btnVoltar.Size = new System.Drawing.Size(75, 36);
+            this.btnVoltar.TabIndex = 5;
+            this.btnVoltar.Text = "Voltar";
+            this.btnVoltar.UseVisualStyleBackColor = true;
+            this.btnVoltar.Click += new System.EventHandler(this.BtnVoltar_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(479, 339);
+            this.Controls.Add(this.btnVoltar);
+            this.Controls.Add(this.btnConsultar);
+            this.Controls.Add(this.cboOpcao);
+            this.Controls.Add(this.lblRes);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblRes;
+        private System.Windows.Forms.ComboBox cboOpcao;
+        private System.Windows.Forms.Button btnConsultar;
+        private System.Windows.Forms.Button btnVoltar;
+    }
+}
---
+++ Projetos/Switch_case/Form2.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Switch_case
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnConsultar_Click(object sender, EventArgs e)
+        {
+            switch (cboOpcao.Text)
+            /*De acordo com o final da placa escolhida no combobox será
+            exibida uma mensagem informativa.
+            Caso o final da placa seja 1 ou 2 o rodízio será segunda, caso
+            3 ou 4 na terça e assim por diante.*/
+            {
+                case "1 ou 2":
+                    lblRes.Text = "O Dia de seu rodízio é Segunda-Feira!";
+                    break;
+                case "3 ou 4":
+                    lblRes.Text = " O Dia de seu rodízio é Terça-Feira!";
+                    break;
+                case "5 ou 6":
+                    lblRes.Text = " O Dia de seu rodízio é Quarta-Feira!";
+                    break;
+                case "7 ou 8":
+                    lblRes.Text = "O Dia de seu rodízio é Quinta-Feira!";
+                    break;
+                case "9 ou 0":
+                    lblRes.Text = "O Dia de seu rodízio é Sexta-Feira!";
+                    break;
+            }
+        }
+
+        private void BtnVoltar_Click(object sender, EventArgs e)
+        {
+            Form1 novo = new Form1();
+            //Apresenta o formulário e chama em seguida
+            novo.Show();
+            //fecha o formulário atual
+            this.Visible = false;
+        }
+
+        private void CboOpcao_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
