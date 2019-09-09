36fb506deeb22fbbe48dda218e53965187804449
Rafael Teixeira <fael.teixeira@gmail.com>
2019-09-09 03:21:43 -0300

    Add 'sistema cantina' (C#)

---
+++ Projetos/Sistema_Cantina/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Sistema_Cantina
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lstCaixa = new System.Windows.Forms.ListBox();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.picImagem = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picImagem)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(197, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Sistema Cantina";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(81, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Código do Produto";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(81, 283);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Valor Total:";
+            // 
+            // lstCaixa
+            // 
+            this.lstCaixa.FormattingEnabled = true;
+            this.lstCaixa.Location = new System.Drawing.Point(218, 106);
+            this.lstCaixa.Name = "lstCaixa";
+            this.lstCaixa.Size = new System.Drawing.Size(178, 160);
+            this.lstCaixa.TabIndex = 3;
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(84, 132);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(128, 20);
+            this.txtCodigo.TabIndex = 4;
+            this.txtCodigo.TextChanged += new System.EventHandler(this.TxtCodigo_TextChanged);
+            // 
+            // picImagem
+            // 
+            this.picImagem.Location = new System.Drawing.Point(84, 159);
+            this.picImagem.Name = "picImagem";
+            this.picImagem.Size = new System.Drawing.Size(128, 105);
+            this.picImagem.TabIndex = 5;
+            this.picImagem.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(476, 359);
+            this.Controls.Add(this.picImagem);
+            this.Controls.Add(this.txtCodigo);
+            this.Controls.Add(this.lstCaixa);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picImagem)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox lstCaixa;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.PictureBox picImagem;
+    }
+}
+
---
+++ Projetos/Sistema_Cantina/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sistema_Cantina
+{
+    public partial class Form1 : Form
+    {
+        string[] produtos = new string[10];
+        string[] codigo = new string[10];
+        double[] valor = new double[10];
+        double soma;
+        /*
+        Declaração de 3 arrays para armazenar 10 produtos,
+        e seus respectivos códigos e valores.
+        */
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void TxtCodigo_TextChanged(object sender, EventArgs e)
+        {
+            if (txtCodigo.Text.Length == 3)
+            {
+                int indice = 0;
+                for (int prod = 0; prod < codigo.Length; prod++)
+                {
+                    if (txtCodigo.Text == codigo[prod])
+                    {
+                        indice = prod;
+                    }
+                }
+                if (indice == 0)
+                {
+                    MessageBox.Show("Produto não encontrado.");
+                }
+                else
+                {
+                    lstCaixa.Items.Add(txtCodigo.Text+" -- "+produtos[indice]+" -- R$ "+valor[indice]);
+                    soma += valor[indice];
+                    label3.Text = "Valor Total = R$ " + soma;
+                    picImagem.ImageLocation = "c:/imagens/" + codigo[indice] + ".jpg";
+                    txtCodigo.Text = "";
+                    txtCodigo.Focus();
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            carregarArray();
+            soma = 0;
+        }
+        /*Ao entrar no Form Load será chamado o método*/
+        private void carregarArray()
+        /*Criação do Método para ser chamado quando necessário, isso evita repetir
+        várias vezes o mesmo código*/
+        {
+            codigo[1] = "001";
+            codigo[2] = "002";
+            codigo[3] = "003";
+            codigo[4] = "004";
+            codigo[5] = "005";
+            produtos[1] = "Pastel";
+            produtos[2] = "Coxinha";
+            produtos[3] = "Hot_Dog";
+            produtos[4] = "Chocolate";
+            produtos[5] = "Suco";
+            valor[1] = 6.00;
+            valor[2] = 5.00;
+            valor[3] = 12.00;
+            valor[4] = 3.50;
+            valor[5] = 8.00;
+            /*Códigos, Produtos e Valores ref. os arrays que foram declarados*/
+        }
+    }
+}
