410c13d284a8a956aede6417cfdab0d5e5df382d
Rafael Teixeira <fael.teixeira@gmail.com>
2019-10-29 02:41:44 -0300

    Add projeto 'Loja' (C#, .NET, SQL Server)

---
+++ Projetos/Loja/Loja.DTO/usuario_DTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Loja.DTO
+{
+    public class usuario_DTO
+    {
+        public int cod_usuario { get; set; }
+        public string nome { get; set; }
+        public string login { get; set; }
+        public string email { get; set; }
+        public string senha { get; set; }
+        public DateTime cadastro { get; set; }
+        public string situacao { get; set; }
+        public int perfil { get; set; }
+    }
+}
