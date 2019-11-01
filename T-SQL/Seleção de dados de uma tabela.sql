01460d0757dcf8b4df828902333415ab0a133ee0
Rafael Teixeira <fael.teixeira@gmail.com>
2019-10-31 21:54:07 -0300

    Add exemplos de SELECT (T-SQL)

---
+++ T-SQL/Seleção de dados de uma tabela.sql
@@ -0,0 +1,5 @@
+-- Exemplo de SELECT mostrando apenas alguns campos da tabela Customers
+select id, name, salary from Customers;
+
+-- Exemplo de SELECT mostrando todos os campos da tabela Customers
+select * from Customers;
