b732f689ac323dbd93dd965f20f198d85b6358d9
Rafael Teixeira <fael.teixeira@gmail.com>
2019-11-07 22:50:29 -0300

    Add exemplos de cláusula WHERE (T-SQL)

---
+++ T-SQL/Uso da cláusula WHERE.sql
@@ -0,0 +1,7 @@
+-- Nesta consulta serão mostrados somente aqueles
+-- que tiverem salário maior que 2000.
+SELECT id, name, salary FROM customers WHERE salary>2000;
+
+-- Já nesta será(ão)  mostrado(s) aqueles que tiverem
+-- o nome igual a "Hardik".
+SELECT id, name, salary FROM customers WHERE name='Hardik';
