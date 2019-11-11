4db069bbaf0272f5c8905b24dc0feecb8ce823a5
Rafael Teixeira <fael.teixeira@gmail.com>
2019-11-10 23:41:44 -0300

    Add exemplos de cláusula ORDER BY (T-SQL)

---
+++ T-SQL/Ordenação de resultados de um SELECT.sql
@@ -0,0 +1,7 @@
+-- Está ordenando os registros da tabela Customers
+-- pelo nome, e em caso de igualdade, pelo salário.
+SELECT * FROM Customers ORDER BY name, salary;
+
+-- Está ordenando os registros da tabela Customers
+-- pelo nome, de maneira decrescente.
+SELECT * FROM Customers ORDER BY name DESC;
