7529f0210f10a80ec25346223581e259b767f68c
Rafael Teixeira <fael.teixeira@gmail.com>
2019-12-16 21:38:44 -0300

    Add exemplo de função ATAN2 (SQL).

---
+++ SQL/Função ATAN2.sql
@@ -0,0 +1,5 @@
+-- Exemplo da função ATAN2.
+
+-- A função, dados 2 números (n1 e n2), retornará o arco tangente de
+-- (n1/n2). Neste caso será retornado o arco tangente de 0.5 (3/6).
+SELECT ATAN2(3,6);
