14016aa80d372d027b2ff2571fef63f1c91b9283
Rafael Teixeira <fael.teixeira@gmail.com>
2020-02-12 00:00:46 -0300

    Add exemplo de função LOG (SQL).

---
+++ SQL/Função LOG.sql
@@ -0,0 +1,8 @@
+-- Serão mostrados 2 exemplos de uso da função LOG.
+
+-- Neste primeiro, será retornado o LOG de 45 na base E.
+SELECT LOG(45);
+
+-- Já neste outro exemplo, será retornado o LOG de 65536
+-- na base 2.
+SELECT LOG(2,65536);
