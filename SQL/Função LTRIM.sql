a15c45e5575166d945e044e23b595d0cca683afa
Rafael Teixeira <fael.teixeira@gmail.com>
2020-03-28 04:16:42 +0000

    Add exemplo de função LTRIM (SQL).

---
+++ SQL/Função LTRIM.sql
@@ -0,0 +1,6 @@
+-- Função LTRIM
+-- Essa função remove espaços em brancos que existem no início de uma string, 
+-- antes da primeira letra/número/caractere especial.
+
+-- Neste exemplo, será retornada a string '  barbar' sem os dois espaços em brancos que tem antes de 'barbar'.
+SELECT LTRIM('  barbar');
