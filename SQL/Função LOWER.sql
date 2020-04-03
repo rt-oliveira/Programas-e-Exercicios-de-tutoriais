9538b6e819f4e295f0d233aa47410c0af4baf93d
Rafael Teixeira <fael.teixeira@gmail.com>
2020-04-03 03:11:17 +0000

    Add exemplo de função LOWER (SQL).

---
+++ SQL/Função LOWER.sql
@@ -0,0 +1,6 @@
+-- Função LOWER
+-- Converte todas as letras da string para a caixa baixa (letras minúsculas)
+
+-- Neste exemplo, a string QUADRATICALLY terá todas as suas letras convertidas para
+-- minúsculas, retornando 'quadratically'.
+SELECT LOWER('QUADRATICALLY');
