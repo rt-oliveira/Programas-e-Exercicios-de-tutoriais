de4ee90828c1a82e7668c484bf1d49b51f5cc34e
Rafael Teixeira <fael.teixeira@gmail.com>
2020-04-05 06:15:27 +0000

    Add exemplo de função UPPER (SQL).

---
+++ SQL/Função UPPER.sql
@@ -0,0 +1,5 @@
+-- Função UPPER
+-- Converte todos os caracteres da string para letras maiúsculas.
+
+-- Neste exemplo, a string passada como parâmetro vai ter todas as suas letras colocadas em maiúsculo.
+SELECT UPPER('Allah-hus-samad');
