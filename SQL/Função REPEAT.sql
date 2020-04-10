e217a224d58cf5a71186f675c45c44bc4fc81252
Rafael Teixeira <fael.teixeira@gmail.com>
2020-04-10 02:37:13 -0300

    Add exemplo de função REPEAT (SQL).

---
+++ SQL/Função REPEAT.sql
@@ -0,0 +1,6 @@
+-- Função REPEAT
+-- Dado uma string S eum número N, retornará uma outra string contendo S repetido N vezes.
+
+-- Neste exemplo será mostrada uma string na qual contém 
+-- 'SQL' repetido 3 vezes.
+SELECT REPEAT('SQL', 3);
