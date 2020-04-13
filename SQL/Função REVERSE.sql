39735c4174e1448a264c0f0e1ac9d40bdeb76842
Rafael Teixeira <fael.teixeira@gmail.com>
2020-04-13 00:02:54 -0300

    Add exemplo de função REVERSE (SQL).

---
+++ SQL/Função REVERSE.sql
@@ -0,0 +1,5 @@
+-- Função REVERSE
+-- Dado uma string, retorna a mesma invertida, isto é, com os seus caracteres em ordem invertida.
+
+-- Neste exemplo, a string 'abcd' virará 'dcba'.
+SELECT REVERSE('abcd');
