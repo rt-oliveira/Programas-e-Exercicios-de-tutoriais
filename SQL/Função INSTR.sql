b2c7eab30d82b8dd35e12b3dd90061c0fed5db51
Rafael Teixeira <fael.teixeira@gmail.com>
2020-04-22 00:15:31 -0300

    Add exemplo de função INSTR (SQL).

---
+++ SQL/Função INSTR.sql
@@ -0,0 +1,6 @@
+-- Função INSTR
+-- Retorna o índice da primeira ocorrência de uma determinada string em outra.
+-- Caso não encontre nenhuma ocorrência, retorna 0.
+
+-- Neste exemplo, será mostrado o índice da primeira ocorrência da string 'bar' na string 'foobarbar'.
+SELECT INSTR('foobarbar','bar');
