1b16fadb27bb7a966331b9760c66a2e16d3fccea
Rafael Teixeira <fael.teixeira@gmail.com>
2020-05-04 00:23:45 -0300

    Add exemplo de função CHAR_LENGTH (SQL).

---
+++ SQL/Função CHAR_LENGTH.sql
@@ -0,0 +1,5 @@
+-- Função CHAR_LENGTH
+-- Retorna a quantidade de caracteres de uma string.
+
+-- Neste exemplo, será mostrada a quantidade de caracteres da string 'text'
+SELECT CHAR_LENGTH('text');
