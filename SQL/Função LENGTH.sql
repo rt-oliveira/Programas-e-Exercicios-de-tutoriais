12c5ef677cbb65b3058cc88d922a3b91f51a060a
Rafael Teixeira <fael.teixeira@gmail.com>
2020-05-12 00:35:28 -0300

    Add exemplo de função LENGTH (SQL).

---
+++ SQL/Função LENGTH.sql
@@ -0,0 +1,6 @@
+-- Função LENGTH
+-- Retorna o tamanho da string em caracteres.
+-- Ou seja, por exemplo, um caractere que ocupa 2 bytes será contado como 2.
+
+-- Neste exemplo, é mostrado o tamanho em bytes da string 'text'.
+SELECT LENGTH('text');
