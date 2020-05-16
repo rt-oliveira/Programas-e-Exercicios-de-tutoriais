e956ec463d749cf3cefc57db9451a18248a1f0ea
Rafael Teixeira <fael.teixeira@gmail.com>
2020-05-16 00:34:32 -0300

    Add mais um exemplo de função CONCAT (SQL).

---
+++ SQL/Função CONCAT 2.sql
@@ -0,0 +1,8 @@
+-- Função CONCAT
+-- Dada uma quantidade de strings passadas como parâmetro, ela
+-- retornará uma única string, contendo todas as strings passadas
+-- concatenadas.
+
+-- Neste exemplo, as string 'My', 'S' e 'QL' vão se tornar uma única
+-- string contendo 'MySQL'.
+SELECT CONCAT('My','S','QL');
