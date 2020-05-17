3a7e8061abf4c746c2be08d434edf5f105b7a476
Rafael Teixeira <fael.teixeira@gmail.com>
2020-05-17 04:13:41 -0300

    Add exemplo de função FIELD (SQL).

---
+++ SQL/Função FIELD.sql
@@ -0,0 +1,8 @@
+-- Função FIELD
+-- Dada uma primeira string (S) e uma lista de strings (L), a função retornará
+-- o índice da S em L (começando em 1).
+-- Caso não encontre, a função retorna 0.
+
+-- Neste exemplo, é mostrado que a string 'ej' é a segunda da lista de strings
+-- passadas.
+SELECT FIELD('ej','Hej','ej','Heja','hej','foo');
