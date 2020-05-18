4d6ad42ddd846d41e1550eddc79c56a8019c8ea6
Rafael Teixeira <fael.teixeira@gmail.com>
2020-05-18 00:47:48 -0300

    Add exemplo de função QUOTE (SQL).

---
+++ SQL/Função QUOTE.sql
@@ -0,0 +1,11 @@
+-- Função QUOTE
+-- Dado uma string S, essa função retornará S entre
+-- aspas simples.
+
+-- Essa função pode ser boa para gerar resultados que
+-- podem ser usados em comandos SQL.
+
+-- Neste exemplo, dado a string passada como parâmetro,
+-- será retornado a mesma entre aspas simples.
+SELECT QUOTE('Don\'t');
+-- Aqui foi passado (Don\'t) e voltará ('Don\'t').
