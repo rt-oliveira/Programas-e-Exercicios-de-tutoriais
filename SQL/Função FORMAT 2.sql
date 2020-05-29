17e018a18580d25f09ea59a72f185b3c64eed832
Rafael Teixeira <fael.teixeira@gmail.com>
2020-05-29 00:01:02 -0300

    Add novo exemplo de função FORMAT (SQL).

---
+++ SQL/Função FORMAT 2.sql
@@ -0,0 +1,7 @@
+-- Função FORMAT
+-- Formata um número, com agrupamento de dígitos,
+-- para uma determinada quantidade de casas decimais.
+
+-- Nesse exemplo, formata o número passado para
+-- 4 casas decimais.
+SELECT FORMAT(12332.123456, 4);
