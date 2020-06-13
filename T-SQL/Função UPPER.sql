97acdc44e5ef99f8001e857d35057fbc70450cfc
Rafael Teixeira <fael.teixeira@gmail.com>
2020-06-13 02:00:00 -0300

    Add exemplo de função UPPER (T-SQL).

---
+++ T-SQL/Função UPPER.sql
@@ -0,0 +1,5 @@
+-- Função UPPER
+-- Coloca todos os caracteres de uma string em letras maiúsculas.
+
+-- Nesse exemplo, a string 'SqlServer' vai se transformar em 'SQLSERVER'.
+SELECT UPPER('SqlServer');
