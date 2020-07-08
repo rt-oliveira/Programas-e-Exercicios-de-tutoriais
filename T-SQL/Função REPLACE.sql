c328614da9139b6776cb1f2ac60a93844081684f
Rafael Teixeira <fael.teixeira@gmail.com>
2020-07-08 00:45:33 -0300

    Add exemplo de função REPLACE (T-SQL).

---
+++ T-SQL/Função REPLACE.sql
@@ -0,0 +1,5 @@
+-- Função REPLACE
+-- Substitui todas as ocorrências de uma substring por uma outra em uma determinada string.
+
+-- Neste exemplo todas as ocorrências de 'I' serão substituídas por 'K'.
+SELECT REPLACE('INDIA','I','K');
