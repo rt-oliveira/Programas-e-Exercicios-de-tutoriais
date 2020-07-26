8e1fda68ae4ea437a77d3f4dd56d027eb0064f1d
Rafael Teixeira <fael.teixeira@gmail.com>
2020-07-26 03:51:38 -0300

    Add exemplo de função SUBSTRING (T-SQL).

---
+++ T-SQL/Função SUBSTRING.sql
@@ -0,0 +1,8 @@
+-- Função SUBSTRING
+-- Retorna um parte de uma string, dado uma posição inicial e
+-- uma quantidade de caracteres
+
+-- Abaixo haverão 3 exemplos dessa função:
+SELECT SUBSTRING('WORLD', 1, 3);
+SELECT SUBSTRING('INDIA', 3, 3);
+SELECT SUBSTRING('KING', 2, 3);
