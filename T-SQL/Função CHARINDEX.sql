d8bee7d2e2ed05004a18b04aa8850556cb9f50b5
Rafael Teixeira <fael.teixeira@gmail.com>
2020-07-27 22:50:58 -0300

    Add exemplo de função CHARINDEX (T-SQL).

---
+++ T-SQL/Função CHARINDEX.sql
@@ -0,0 +1,6 @@
+-- Função CHARINDEX
+-- Retorna o índice da primeira ocorrência de uma determinada string em outra.
+-- Observação: essa indexação é a partir de 1. Caso não encontre, retorna 0.
+
+-- Neste exemplo, será mostrado o índice da primeira ocorrência de G em KING.
+SELECT CHARINDEX('G', 'KING');
