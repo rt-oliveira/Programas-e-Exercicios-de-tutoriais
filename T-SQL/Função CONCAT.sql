fa7cd8844a1fc1e19a74c58c1e0fb0d1bd12f4d4
Rafael Teixeira <fael.teixeira@gmail.com>
2020-07-31 00:41:34 -0300

    Add exemplo de função CONCAT (T-SQL).

---
+++ T-SQL/Função CONCAT.sql
@@ -0,0 +1,5 @@
+-- Função CONCAT
+-- Dado uma lista de strings, a função retorna uma única string composta pela concatenação das strings da lista.
+
+-- Neste exemplo, será gerado a string concatenada 'A,B,C'.
+SELECT CONCAT('A', ',', 'B', ',', 'C');
