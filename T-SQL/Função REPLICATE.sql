a71d854db6852acb85285fc398f5ab2cfb68d27b
Rafael Teixeira <fael.teixeira@gmail.com>
2020-08-30 01:49:20 -0300

    Add função REPLICATE (T-SQL).

---
+++ T-SQL/Função REPLICATE.sql
@@ -0,0 +1,5 @@
+-- Função REPLICATE
+-- Dado uma string S e um número N, a função retornará uma outra string que terá S repetido N vezes.
+
+-- Neste exemplo, será retornado 'WORLDWORLD'.
+SELECT REPLICATE('WORLD', 2);
