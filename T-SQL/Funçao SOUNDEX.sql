b6a401fb9a3ac714ca591e5cf28011866407bc1e
Rafael Teixeira <fael.teixeira@gmail.com>
2020-09-03 23:46:39 -0300

    Add exemplo de função SOUNDEX (T-SQL).

---
+++ T-SQL/Funçao SOUNDEX.sql
@@ -0,0 +1,8 @@
+-- Função SOUNDEX
+-- Retorna o código Soundex de uma string.
+--
+-- Para mais informações sobre o algoritmo Soundex:
+-- https://en.wikipedia.org/wiki/Soundex
+
+-- Neste exemplo, serão mostrados os códigos soundex das strings 'Smith' e 'Smyth'.
+SELECT SOUNDEX('Smith'), SOUNDEX('Smyth');
